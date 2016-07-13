8b7f55154770323edde7da49f8cbd14246454043
Alan Accurso <accurso.alan@gmail.com>
2016-07-13 18:00:57 -0300

    Favors class toggle for visibility over inline styling

--- live-coding/js/main.js
+++ live-coding/js/main.js
@@ -2,11 +2,6 @@
     var menuToggleButton = document.getElementsByClassName('js-menu-toggle-button')[0];
     var menu = document.getElementsByClassName('js-menu')[0];
     menuToggleButton.onclick = function() {
-        var currentDisplay = menu.style.display || 'none';
-        if (currentDisplay === 'none') {
-            menu.style.display = 'block';
-        } else {
-            menu.style.display = 'none';
-        }
+        menu.classList.toggle('visible');
     };
 })();
